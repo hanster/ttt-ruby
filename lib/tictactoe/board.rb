4a1ab4db910d249ba6a90220a0aec9ee9247a6f3
Sam Han <shan@8thlight.com>
2015-05-06 17:55:51 +0100

    Handle win cases and move concept in board

--- lib/tictactoe/board.rb
+++ lib/tictactoe/board.rb
@@ -4,6 +4,10 @@ module TicTacToe
     BOARD_SIZE = BOARD_DIM * BOARD_DIM
     X_MARKER = 'X'
     O_MARKER = 'O'
+    VERTICAL_WIN_CASES = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    HORIZONAL_WIN_CASES = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    DIAGONAL_WIN_CASES = [[0, 4, 8], [2, 4, 6]]
+    ALL_WIN_CASES = VERTICAL_WIN_CASES + HORIZONAL_WIN_CASES + DIAGONAL_WIN_CASES
 
     def initialize(initial_positions = '-' * BOARD_SIZE, initial_turn = X_MARKER)
       @turn = initial_turn
@@ -27,12 +31,36 @@ module TicTacToe
       end
     end
 
+    def game_over?
+      no_more_moves? || has_anyone_won?
+    end
+
+    def has_won?(marker)
+      marker_positions = positions_with_mark(marker)
+      ALL_WIN_CASES.each do |win_case|
+        return true if (win_case - marker_positions).empty?
+      end
+      false
+    end
+
     private
 
     attr_accessor :positions, :turn
 
+    def positions_with_mark(marker)
+      positions.each_index.select { |index| positions[index] == marker }
+    end
+
     def next_turn(current_turn)
       current_turn == X_MARKER ? O_MARKER : X_MARKER
+    end
+
+    def has_anyone_won?
+      has_won?(X_MARKER) || has_won?(O_MARKER)
+    end
+
+    def no_more_moves?
+      available_positions.count == 0
     end
   end
 end
--- spec/tictactoe/board_spec.rb
+++ spec/tictactoe/board_spec.rb
@@ -32,5 +32,61 @@ module TicTacToe
       board = Board.new()
       expect(board.available_positions).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
     end
+
+    describe "#game_over?" do
+      it "isn't game over when there is a new board" do
+        board = Board.new()
+        expect(board.game_over?).to be false
+      end
+
+      it "is game over when there are no more moves left" do
+        board = Board.new('XXXXXXXXX')
+        expect(board.game_over?).to be true
+      end
+
+      it "is game over when a player has won" do
+        board = Board.new('XXX-OO---')
+        expect(board.game_over?).to be true
+      end
+    end
+
+    describe "#has_won?" do
+      context "not won" do
+        it "No wins yet" do
+          board = Board.new
+          expect(board.has_won?('X')).to be false
+          expect(board.has_won?('O')).to be false
+          board = Board.new('OXXXXOOOX')
+          expect(board.has_won?('X')).to be false
+          expect(board.has_won?('O')).to be false
+        end
+      end
+      context "player has won" do
+        it "vertical cases" do
+          board = Board.new('XO-XX-X--')
+          expect(board.has_won?('X')).to be true
+          board = Board.new('-X--X--X-')
+          expect(board.has_won?('X')).to be true
+          board = Board.new('--X--X--X')
+          expect(board.has_won?('X')).to be true
+        end
+
+        it "horizontal cases" do
+          board = Board.new('OOO------')
+          expect(board.has_won?('O')).to be true
+          board = Board.new('---OOO---')
+          expect(board.has_won?('O')).to be true
+          board = Board.new('------OOO')
+          expect(board.has_won?('O')).to be true
+        end
+
+        it "diagonal cases" do 
+          board = Board.new('X---X---X')
+          expect(board.has_won?('X')).to be true
+          board = Board.new('--O-O-O--')
+          expect(board.has_won?('O')).to be true
+        end
+      end
+    end
   end
 end
