23b7a5ad1f78ffaf2b5a6b6177ae711dfbe211c6
Sam Han <shan@8thlight.com>
2015-06-19 11:46:35 +0100

    Move creation of gui objects into the GraphicalUi class

--- lib/tictactoe/qt_game.rb
+++ lib/tictactoe/qt_game.rb
@@ -4,10 +4,6 @@ require 'tictactoe/marker'
 require 'tictactoe/factory/players_factory'
 require 'tictactoe/factory/board_factory'
 require 'tictactoe/ai/minimax_ai'
-require 'tictactoe/game_types'
-require 'tictactoe/ui/gui_builder'
-require 'tictactoe/ui/menu_group'
-require 'tictactoe/ui/gui_board'
 require 'tictactoe/ui/graphical_ui'
 
 module TicTacToe
@@ -15,8 +11,6 @@ module TicTacToe
     X_MARKER_COLOR = "color: red"
     O_MARKER_COLOR = "color: blue"
     TICTACTOE = 'TicTacToe'
-    PLAY_BUTTON_TEXT = 'Play'
-    PLAY_BUTTON_NAME = 'play_button'
     GAME_BOARD_NAME = 'game_board'
     GAME_TYPES_TEXT = 'Game Types'
     BOARD_TYPES_TEXT = 'Board Types'
@@ -30,27 +24,7 @@ module TicTacToe
       setWindowTitle(TICTACTOE)
       resize(600, 600)
       @ai = Ai::MinimaxAi.new
-      @ui = Ui::GraphicalUi.new
-      build_gui_objects
-      @ui.add_label(@info_label)
-    end
-
-    def build_gui_objects
-      @gui_builder = Ui::GuiBuilder.new(self)
-
-      @players_menu = Ui::MenuGroup.new(GAME_TYPES_TEXT, GameTypes::get_player_options)
-      @board_menu = Ui::MenuGroup.new(BOARD_TYPES_TEXT, GameTypes::get_board_options)
-      @gui_board = Ui::GuiBoard.new
-      @gui_board.register_panel_on_click(self, :clicked)
-
-      @info_label = @gui_builder.create_label
-      play_button = @gui_builder.create_button(PLAY_BUTTON_NAME, PLAY_BUTTON_TEXT, :play_new_game)
-      @set_up_grid = Qt::GridLayout.new(self)
-      @set_up_grid.addLayout(@gui_board, 1, 0, 3, 3)
-      @set_up_grid.addWidget(@players_menu.group_box, 0, 0)
-      @set_up_grid.addWidget(@board_menu.group_box, 0, 1)
-      @set_up_grid.addWidget(play_button, 0, 2)
-      @set_up_grid.addWidget(@info_label, 4, 0)
+      @ui = Ui::GraphicalUi.new(self)
     end
 
     def play_new_game
@@ -62,17 +36,17 @@ module TicTacToe
       board = set_up_new_board
       players = set_up_players
 
-      @gui_board.new_board(board.dimension)
+      @ui.new_gui_board(board)
       @game = Game.new(board, players, @ui)
     end
 
     def set_up_players
-      players_selection = @players_menu.selected_option
+      players_selection = @ui.get_players_selection
       players = Factory::PlayersFactory.new(@ui, @ai).create_from_string(players_selection)
     end
 
     def set_up_new_board
-      board_selection = @board_menu.selected_option
+      board_selection = @ui.get_board_selection
       Factory::BoardFactory.new.create_from_string(board_selection)
     end
 
@@ -93,9 +67,9 @@ module TicTacToe
     def update_game
       @game.draw
       unless @game.game_over?
-        @info_label.text = player_turn_message
+        @ui.set_info_label(player_turn_message)
         next_move = get_player_move
-        @gui_board.click_panel(next_move) if next_move
+        @ui.click_board_panel(next_move) if next_move
       end
     end
 
--- lib/tictactoe/ui/graphical_ui.rb
+++ lib/tictactoe/ui/graphical_ui.rb
@@ -1,11 +1,61 @@
+require 'tictactoe/ui/menu_group'
+require 'tictactoe/game_types'
+require 'tictactoe/ui/gui_builder'
+require 'tictactoe/ui/gui_board'
+
 module TicTacToe
   module Ui
     class GraphicalUi
       WINNER_MESSAGE = "%s wins!"
       DRAW_MESSAGE = "It's a draw!"
-      def add_label(info_label)
-        @info_label = info_label
+      GAME_TYPES_TEXT = 'Game Types'
+      BOARD_TYPES_TEXT = 'Board Types'
+      PLAY_BUTTON_TEXT = 'Play'
+      PLAY_BUTTON_NAME = 'play_button'
+
+      def initialize(parent)
+        @parent = parent
+        build_gui_objects
       end
+
+      def build_gui_objects
+        @gui_builder = GuiBuilder.new(@parent)
+
+        @players_menu = Ui::MenuGroup.new(GAME_TYPES_TEXT, GameTypes::get_player_options)
+        @board_menu = Ui::MenuGroup.new(BOARD_TYPES_TEXT, GameTypes::get_board_options)
+        @gui_board = Ui::GuiBoard.new
+        @gui_board.register_panel_on_click(@parent, :clicked)
+
+        @info_label = @gui_builder.create_label
+        play_button = @gui_builder.create_button(PLAY_BUTTON_NAME, PLAY_BUTTON_TEXT, :play_new_game)
+        set_up_grid = Qt::GridLayout.new(@parent)
+        set_up_grid.addLayout(@gui_board, 1, 0, 3, 3)
+        set_up_grid.addWidget(@players_menu.group_box, 0, 0)
+        set_up_grid.addWidget(@board_menu.group_box, 0, 1)
+        set_up_grid.addWidget(play_button, 0, 2)
+        set_up_grid.addWidget(@info_label, 4, 0)
+      end
+
+      def get_players_selection
+        @players_menu.selected_option
+      end
+
+      def get_board_selection
+        @board_menu.selected_option
+      end
+
+      def set_info_label(text)
+        @info_label.text = text
+      end
+
+      def new_gui_board(board)
+        @gui_board.new_board(board.dimension)
+      end
+
+      def click_board_panel(panel_number)
+        @gui_board.click_panel(panel_number) 
+      end
+
       def prompt_for_move(board, marker)
 
       end
